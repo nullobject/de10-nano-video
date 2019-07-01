133aa67c6bf6274c60d2ba526fc3a941b2f2ac1e
Joshua Bassett <hello@joshbassett.info>
2019-07-01 21:12:58 +1000

    Try Pickin video timings

--- src/sync_gen.vhd
+++ src/sync_gen.vhd
@@ -4,75 +4,72 @@ use ieee.numeric_std.all;
 
 entity sync_gen is
   port(
-    -- clock
     clk : in std_logic;
-
-    -- clock enable
     cen : in std_logic;
-
-    -- horizontal and vertical position
-    hpos, vpos : out unsigned(8 downto 0);
-
-    -- horizontal and vertical sync
     hsync, vsync : out std_logic;
-
-    -- horizontal and vertical blank
-    hblank, vblank : out std_logic
+    hblank, vblank : out std_logic;
+    tile_x, tile_y : out std_logic_vector(4 downto 0);
+    pixel_x, pixel_y : out std_logic_vector(2 downto 0)
   );
 end sync_gen;
 
 architecture struct of sync_gen is
-  signal hcnt : unsigned(8 downto 0) := 9x"080";
-  signal vcnt : unsigned(8 downto 0) := 9x"0f8";
-
-  signal do_hsync : boolean;
+  signal hcnt   : unsigned(8 downto 0) := to_unsigned(511, 9);
+  signal vcnt   : unsigned(8 downto 0) := to_unsigned(511, 9);
+  signal vcnt_r : unsigned(8 downto 0) := to_unsigned(511, 9);
 begin
-  hv_count : process(clk)
+  tile_x  <= std_logic_vector(hcnt(7 downto 3));
+  tile_y  <= std_logic_vector(vcnt_r(7 downto 3));
+  pixel_x <= std_logic_vector(hcnt(2 downto 0));
+  pixel_y <= std_logic_vector(vcnt_r(2 downto 0));
+
+  -- Horizontal counter: 511-128 + 1 = 384 pixels
+  -- 128 to 175: 48 pixels end of line
+  -- 176 to 255: 80 pixels beginning of line
+  -- 256 to 511: 256 pixels displayed (32 tiles)
+
+  -- Vertical counter: 511-248 + 1 = 264 lines
+  -- 496 to 511: 16 lines end of frame
+  -- 248 to 271: 24 lines beginning of frame
+  -- 272 to 495: 224 lines displayed (28 tiles)
+
+  -- Horizontal sync: hcnt = [176 to 207] (32 pixels)
+  -- Vertical sync: vcnt = [248 to 255] (8 lines)
+  process(clk)
   begin
     if rising_edge(clk) then
       if cen = '1' then
-        -- horizontal counter counts $080 to $1ff = 384 (6Mhz/384 = 15.625 kHz)
-        if hcnt = 9x"1ff" then
-          hcnt <= 9x"080";
+        if hcnt = 511 then
+          hcnt <= to_unsigned(128, 9);
+          vcnt_r <= vcnt;
         else
           hcnt <= hcnt + 1;
         end if;
 
-        -- vertical counter counts $0f8 to $1ff = 264 (15625/264 = 59.185 Hz)
-        if do_hsync then
-          if vcnt = 9x"1ff" then
-            vcnt <= 9x"0f8";
+        if hcnt = 175 then
+          if vcnt = 511 then
+            vcnt <= to_unsigned(248, 9);
           else
             vcnt <= vcnt + 1;
           end if;
         end if;
-      end if;
-    end if;
-  end process;
 
-  sync : process(clk)
-  begin
-    if rising_edge(clk) then
-      if cen = '1' then
-        if    hcnt = 9x"0af" then hsync <= '1';
-        elsif hcnt = 9x"0cf" then hsync <= '0';
+        if    hcnt = (175+ 0) then hsync <= '0';
+        elsif hcnt = (175+29) then hsync <= '1';
         end if;
 
-        if    hcnt = 9x"08f" then hblank <= '1';
-        elsif hcnt = 9x"0ef" then hblank <= '0';
+        if    vcnt = 511 then vsync <= '0';
+        elsif vcnt = 250 then vsync <= '1';
         end if;
 
-        if do_hsync then
-          if    vcnt = 9x"1ef" then vblank <= '1';
-          elsif vcnt = 9x"10f" then vblank <= '0';
-          end if;
+        if    hcnt = (127+8+1) then hblank <= '1'; -- +8 = delay of the shift register + 1 pixel
+        elsif hcnt = (255+8+1) then hblank <= '0'; -- +8 = delay of the shift register + 1 pixel
+        end if;
+
+        if    vcnt = (495+1+0) then vblank <= '1';
+        elsif vcnt = (271+1+1) then vblank <= '0';
         end if;
       end if;
     end if;
   end process;
-
-  vsync <= not vcnt(8);
-  do_hsync <= (hcnt = 9x"0af");
-  hpos <= hcnt;
-  vpos <= vcnt;
 end architecture;
--- src/video.vhd
+++ src/video.vhd
@@ -14,12 +14,13 @@ entity video is
 end video;
 
 architecture arch of video is
-  signal pll_clk_12 : std_logic;
+  signal pll_clk_6 : std_logic;
   signal pll_locked : std_logic;
 
   signal video_hsync, video_vsync : std_logic;
   signal video_hblank, video_vblank : std_logic;
-  signal video_hpos, video_vpos : unsigned(8 downto 0);
+  signal video_tile_x, video_tile_y : std_logic_vector(4 downto 0);
+  signal video_pixel_x, video_pixel_y : std_logic_vector(2 downto 0);
 
   signal video_on : std_logic;
 begin
@@ -27,26 +28,29 @@ begin
   port map(
     refclk   => clk,
     rst      => '0',
-    outclk_0 => pll_clk_12,
+    outclk_0 => pll_clk_6,
     locked   => pll_locked
   );
 
   sync_gen : entity work.sync_gen
   port map(
-    clk        => pll_clk_12,
-    cen        => '1',
-    hsync      => video_hsync,
-    vsync      => video_vsync,
-    hblank     => video_hblank,
-    vblank     => video_vblank,
-    hpos       => video_hpos,
-    vpos       => video_vpos
+    clk     => pll_clk_6,
+    cen     => '1',
+    hsync   => video_hsync,
+    vsync   => video_vsync,
+    hblank  => video_hblank,
+    vblank  => video_vblank,
+    tile_x  => video_tile_x,
+    tile_y  => video_tile_y,
+    pixel_x => video_pixel_x,
+    pixel_y => video_pixel_y
   );
 
   video_on <= not (video_hblank or video_vblank);
   vga_hs <= not (video_hsync xor video_vsync);
   vga_vs <= '1';
-  vga_r <= "111111" when video_on = '1' and ((video_hpos(2 downto 0) = "000") or (video_vpos(2 downto 0) = "000")) else "ZZZZZZ";
-  vga_g <= "111111" when video_on = '1' and video_hpos(4) = '1' else "ZZZZZZ";
-  vga_b <= "111111" when video_on = '1' and video_vpos(4) = '1' else "ZZZZZZ";
+  -- vga_r <= "111111" when video_on = '1' and ((video_hpos(2 downto 0) = "000") or (video_vpos(2 downto 0) = "000")) else "ZZZZZZ";
+  -- vga_g <= "111111" when video_on = '1' and video_hpos(4) = '1' else "ZZZZZZ";
+  -- vga_b <= "111111" when video_on = '1' and video_vpos(4) = '1' else "ZZZZZZ";
+  vga_r <= "111111" when video_on = '1' and ((video_pixel_x = "000") or (video_pixel_y = "000")) else "ZZZZZZ";
 end arch;
